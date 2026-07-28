754878def0cdc73b970244b452dd72a27feb5570
Can <canbaltaci76@gmail.com>
2026-07-28 11:38:16 +0300

    Google Tag Manager eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,42 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Sinanlar Panjur | Panjur Sistemleri",
-  description:
-    "Sinanlar Panjur: panjur üretim, montaj ve teknik destek. Kurumsal ve güven veren çözümler.",
+  title: "Sinanlar Panjur & Sineklik",
+  description: "Panjur, Sineklik, Kış Bahçesi, Cam Balkon ve Pimapen sistemleri",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="tr"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col bg-black text-zinc-100 selection:bg-orange-500/30 selection:text-orange-50">
+    <html lang="tr">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T8M6GHHR');
+          `}
+        </Script>
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8M6GHHR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
       </body>
     </html>
